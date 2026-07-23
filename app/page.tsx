7a4db0f6b4622303cf37450bb1da6ad93fdec2b3
azhar12102 <azhar12102@hotmail.com>
2026-07-23 20:32:41 +0500

    Describe what you changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
-import Image from "next/image";
-<<<<<<< HEAD
+
 import Hero from "./components/home/hero";
 export default function Home() {
   return (
     <main className="mx-auto max-w-7xl p-10">
       <Hero/>
-=======
-
-export default function Home() {
-  return (
-    <main className="mx-auto max-w-7xl p-10">
-      <h1 className="text-5xl font-bold">
-        Welcome to Mobile Store
-      </h1>
-
-      <p className="mt-5 text-lg">
-        Buy the best mobile accessories at the best prices.
-      </p>
->>>>>>> 05d60906cfe54637124c440f9c3754724835f1fb
     </main>
   );
 }
