581b6bdf56c5b2c79509543149f2ad21a50b7d9a
azhar12102 <azhar12102@hotmail.com>
2026-07-27 11:06:05 +0500

    Some issue on vercel deploy

--- app/components/products/productGrid.tsx
+++ app/components/products/productGrid.tsx
@@ -1,50 +1,50 @@
 import ProductCard from "../ui/productcard";
 
 type Product = {
-  id: number;
-  name: string;
-  image: string;
-  price: number;
-  oldPrice: number;
-  rating: number;
-  discount: number;
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+    oldPrice: number;
+    rating: number;
+    discount: number;
 };
 
 type ProductGridProps = {
-  products: Product[];
+    products: Product[];
 };
 
 export default function ProductGrid({
-  products,
+    products,
 }: ProductGridProps) {
-  if (products.length === 0) {
+    if (products.length === 0) {
+        return (
+            <div className="py-20 text-center">
+                <h2 className="text-2xl font-semibold text-gray-700">
+                    No products found
+                </h2>
+
+                <p className="mt-2 text-gray-500">
+                    Try another search or category.
+                </p>
+            </div>
+        );
+    }
+
     return (
-      <div className="py-20 text-center">
-        <h2 className="text-2xl font-semibold text-gray-700">
-          No products found
-        </h2>
-
-        <p className="mt-2 text-gray-500">
-          Try another search or category.
-        </p>
-      </div>
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {products.map((product) => (
+                <ProductCard
+                    key={product.id}
+                    id={product.id}
+                    name={product.name}
+                    image={product.image}
+                    price={product.price}
+                    oldPrice={product.oldPrice}
+                    rating={product.rating}
+                    discount={product.discount}
+                />
+            ))}
+        </div>
     );
-  }
-
-  return (
-    <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {products.map((product) => (
-      <ProductCard
-  key={product.id}
-  id={product.id}
-  name={product.name}
-  image={product.image}
-  price={product.price}
-  oldPrice={product.oldPrice}
-  rating={product.rating}
-  discount={product.discount}
-/>
-      ))}
-    </div>
-  );
 }
