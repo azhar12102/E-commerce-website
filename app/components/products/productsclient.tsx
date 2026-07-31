120da3eacc7e87302ddd53006ba242de50c06b03
azhar12102 <azhar12102@hotmail.com>
2026-08-01 02:37:16 +0500

    Add productclient file

---
+++ app/components/products/productsclient.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
+import ProductSearch from "./productSearch";
+import ProductFilters from "./productFilters";
+import SortDropdown from "./sortDropdown";
+import ProductGrid from "./productGrid";
+import { products } from "../Data/product";
+
+export default function ProductsPage() {
+  const searchParams = useSearchParams();
+const [searchTerm, setSearchTerm] = useState(
+  searchParams.get("search") || ""
+);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [sortBy, setSortBy] = useState("default");
+useEffect(() => {
+  const search = searchParams.get("search") || "";
+  setSearchTerm(search);
+}, [searchParams]);
+  const filteredProducts = products
+    .filter((product) => {
+      const matchesSearch = product.name
+        .toLowerCase()
+        .includes(searchTerm.toLowerCase());
+
+      const matchesCategory =
+        selectedCategory === "All" ||
+        product.category === selectedCategory;
+
+      return matchesSearch && matchesCategory;
+    })
+    .sort((a, b) => {
+      switch (sortBy) {
+        case "price-low":
+          return a.price - b.price;
+
+        case "price-high":
+          return b.price - a.price;
+
+        case "rating":
+          return b.rating - a.rating;
+
+        case "discount":
+          return b.discount - a.discount;
+
+        default:
+          return 0;
+      }
+    });
+  return (
+    <main className="mx-auto max-w-7xl px-6 py-10">
+      <h1 className="mb-8 text-4xl font-bold">
+        All Products
+      </h1>
+
+      <div className="mb-8">
+        <ProductSearch
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+        />
+      </div>
+
+      <div className="mb-8 flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
+        <ProductFilters
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <SortDropdown
+          sortBy={sortBy}
+          setSortBy={setSortBy}
+        />
+      </div>
+
+      <ProductGrid products={filteredProducts} />
+      
+    </main>
+  );
+}
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,81 +1,10 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import ProductSearch from "../components/products/productSearch";
-import ProductFilters from "../components/products/productFilters";
-import SortDropdown from "../components/products/sortDropdown";
-import ProductGrid from "../components/products/productGrid";
-import { products } from "../components/Data/product";
+import { Suspense } from "react";
+import ProductsClient from "../components/products/productsclient"
 
 export default function ProductsPage() {
-  const searchParams = useSearchParams();
-const [searchTerm, setSearchTerm] = useState(
-  searchParams.get("search") || ""
-);
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [sortBy, setSortBy] = useState("default");
-useEffect(() => {
-  const search = searchParams.get("search") || "";
-  setSearchTerm(search);
-}, [searchParams]);
-  const filteredProducts = products
-    .filter((product) => {
-      const matchesSearch = product.name
-        .toLowerCase()
-        .includes(searchTerm.toLowerCase());
-
-      const matchesCategory =
-        selectedCategory === "All" ||
-        product.category === selectedCategory;
-
-      return matchesSearch && matchesCategory;
-    })
-    .sort((a, b) => {
-      switch (sortBy) {
-        case "price-low":
-          return a.price - b.price;
-
-        case "price-high":
-          return b.price - a.price;
-
-        case "rating":
-          return b.rating - a.rating;
-
-        case "discount":
-          return b.discount - a.discount;
-
-        default:
-          return 0;
-      }
-    });
   return (
-    <main className="mx-auto max-w-7xl px-6 py-10">
-      <h1 className="mb-8 text-4xl font-bold">
-        All Products
-      </h1>
-
-      <div className="mb-8">
-        <ProductSearch
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-        />
-      </div>
-
-      <div className="mb-8 flex flex-col gap-4 lg:flex-row lg:items-center lg:justify-between">
-        <ProductFilters
-          selectedCategory={selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-        />
-
-        <SortDropdown
-          sortBy={sortBy}
-          setSortBy={setSortBy}
-        />
-      </div>
-
-      <ProductGrid products={filteredProducts} />
-      
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ProductsClient />
+    </Suspense>
   );
 }
