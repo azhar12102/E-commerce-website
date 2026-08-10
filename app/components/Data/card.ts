9cd8caf01eb0b11c9c5a663602007b944a1254cf
azhar12102 <azhar12102@hotmail.com>
2026-08-10 21:01:01 +0500

    Featured card changes

--- app/components/Data/card.ts
+++ app/components/Data/card.ts
@@ -2,37 +2,37 @@ export const categories = [
   {
     id: 1,
     name: "Phone Cases",
-    image: "/images/Categories/phonecase.webp",
+    image: "/images/categories/phonecase.webp",
     products: 120,
   },
   {
     id: 2,
     name: "Chargers",
-    image: "/images/Categories/charger.jpg",
+    image: "/images/categories/charger.jpg",
     products: 80,
   },
   {
     id: 3,
     name: "Earbuds",
-    image: "/images/Categories/earbuds.webp",
+    image: "/images/categories/earbuds.webp",
     products: 95,
   },
   {
     id: 4,
     name: "Power Banks",
-    image: "/images/Categories/powerbank.webp",
+    image: "/images/categories/powerbank.webp",
     products: 65,
   },
   {
     id: 5,
     name: "Screen Protectors",
-    image: "/images/Categories/Screen-protector.webp",
+    image: "/images/categories/screen-protector.webp",
     products: 110,
   },
   {
     id: 6,
     name: "Smart Watches",
-    image: "/images/Categories/Smart-watch.webp",
+    image: "/images/categories/smart-watch.webp",
     products: 45,
   },
 ];
